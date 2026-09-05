e1e0036cbffd059fa3ee4d9d7f5a1383dcbafeb3
Shivtej Ghorpade <is23bm033@iitdh.ac.in>
2026-09-06 02:31:37 +0530

    Support DATABASE_URL for production deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,21 @@
 import pg from "pg";
 import "./env.js";
+
 const { Pool } = pg;
 
-const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
-});
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+    : new Pool({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    });
 
 export default pool;
